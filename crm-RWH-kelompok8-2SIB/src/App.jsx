55348f4ec4eb06d9eba22a5a49002cc7a2a8cbb1
Alya verina zahra <alya23si@mahasiswa.pcr.ac.id>
2025-05-21 16:34:12 +0700

    oke

--- crm-RWH-kelompok8-2SIB/src/App.jsx
+++ crm-RWH-kelompok8-2SIB/src/App.jsx
@@ -1,6 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
 import MainLayout from './components/MainLayout';
-import Dashboard from './pages/Dashboard';
 import Penjualan from './pages/Penjualan'; 
 import Produk from './pages/Produk';
 import Laporan from './pages/Laporan';// Pastikan file ini ada
