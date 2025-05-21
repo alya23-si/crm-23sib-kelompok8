6369093e9988b0caf09a399b02339bd128de735e
PaizCMD <faiz23si@mahasiswa.pcr.ac.id>
2025-05-21 15:41:10 +0700

    Sudah siap ketua untuk CRUD pelanggan/customer

--- crm-RWH-kelompok8-2SIB/src/App.jsx
+++ crm-RWH-kelompok8-2SIB/src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import MainLayout from './components/MainLayout';
 import Dashboard from './pages/Dashboard'; // Pastikan file ini ada
+import CustomerManagement from './pages/CustomerManagemenrt';
 
 function App() {
   return (
     <Routes>
       <Route element={<MainLayout />}>
         <Route path="/" element={<Dashboard />} />
+        <Route path="/pelanggan" element={<CustomerManagement />} />
         {/* Tambahkan route lain jika ada */}
       </Route>
     </Routes>
